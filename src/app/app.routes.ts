8540f4117f835643e6713e8da9d5dea3c01a900c
TigasMigas22 <tiagopnsilva@gmail.com>
2025-11-29 23:57:48 +0000

    feat: add detail and edit pages routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { RegisterDegreeComponent } from './pages/admin-dashboard/register-degree
 import { RegisterCourseComponent } from './pages/admin-dashboard/register-course/register-course.component';
 import { RegisterStudentComponent } from './pages/admin-dashboard/register-student/register-student.component';
 import { RegisterTeacherComponent } from './pages/admin-dashboard/register-teacher/register-teacher.component';
+import {DegreeDetailComponent} from './pages/admin-dashboard/degree-detail/degree-detail.component';
+import { CourseDetailComponent } from './pages/admin-dashboard/course-detail/course-detail.component';
+import { StudentDetailComponent } from './pages/admin-dashboard/student-detail/student-detail.component';
+import {TeacherDetailComponent} from './pages/admin-dashboard/teacher-detail/teacher-detail.component';
 
 export const routes: Routes = [
     {
@@ -26,5 +30,21 @@ export const routes: Routes = [
     {
         path: 'admin-dashboard/register-teacher',
         component: RegisterTeacherComponent
+    },
+    {
+        path: 'admin-dashboard/degree/:id', 
+        component: DegreeDetailComponent
+    },
+    {
+        path: 'admin-dashboard/course/:id',
+        component: CourseDetailComponent
+    },
+    {
+        path: 'admin-dashboard/student/:id',
+        component: StudentDetailComponent
+    },
+    {
+        path:'admin-dashboard/teacher/:id',
+        component: TeacherDetailComponent
     }
 ];
