066044af9b8419c56ad2bd6acab3ae483a8419db
Devysson Cardoso <devyssonsc@gmail.com>
2025-12-05 11:40:41 +0000

    fix: reorganization of the path of the admin pages components
    The components have to be inside "pages" folder or "components" folder. If  the component appears on the app.routes.ts, it have to be inside the "pages" folder. If just appears inside another component, it have to be inside the "components" folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { RegisterDegreeComponent } from './pages/admin-dashboard/register-degree/register-degree.component';
-import { RegisterCourseComponent } from './pages/admin-dashboard/register-course/register-course.component';
-import { RegisterStudentComponent } from './pages/admin-dashboard/register-student/register-student.component';
-import { RegisterTeacherComponent } from './pages/admin-dashboard/register-teacher/register-teacher.component';
-import {DegreeDetailComponent} from './pages/admin-dashboard/degree-detail/degree-detail.component';
-import { CourseDetailComponent } from './pages/admin-dashboard/course-detail/course-detail.component';
-import { StudentDetailComponent } from './pages/admin-dashboard/student-detail/student-detail.component';
-import {TeacherDetailComponent} from './pages/admin-dashboard/teacher-detail/teacher-detail.component';
+import { RegisterDegreeComponent } from './pages/register-degree/register-degree.component';
+import { RegisterCourseComponent } from './pages/register-course/register-course.component';
+import { RegisterStudentComponent } from './pages/register-student/register-student.component';
+import { RegisterTeacherComponent } from './pages/register-teacher/register-teacher.component';
+import { DegreeDetailComponent } from './pages/degree-detail/degree-detail.component';
+import { CourseDetailComponent } from './pages/course-detail/course-detail.component';
+import { StudentDetailComponent } from './pages/student-detail/student-detail.component';
+import {TeacherDetailComponent} from './pages/teacher-detail/teacher-detail.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: 'admin-dashboard',
         component: AdminDashboardComponent
     },
-    
     {
-        path: 'admin-dashboard/register-degree', 
+        path: 'admin-dashboard/register-degree',
         component: RegisterDegreeComponent
     },
     {
-        path: 'admin-dashboard/register-course', 
+        path: 'admin-dashboard/register-course',
         component: RegisterCourseComponent
     },
     {
@@ -32,7 +32,7 @@ export const routes: Routes = [
         component: RegisterTeacherComponent
     },
     {
-        path: 'admin-dashboard/degree/:id', 
+        path: 'admin-dashboard/degree/:id',
         component: DegreeDetailComponent
     },
     {
