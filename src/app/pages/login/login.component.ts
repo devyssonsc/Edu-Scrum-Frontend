3a9cf89d9a58fb50915409f77da268264f12419c
teathiago <mcrxdesigner@gmail.com>
2025-12-08 04:38:57 +0000

    feat: login + authentication

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -26,9 +26,10 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       console.log('Formulário Válido (Login):', this.loginForm.value);
-      // this.httpClient.post(`${enviroments.apiUrl}/auth/login`, this.loginForm.value).subscribe(response => {
-      //   console.log('Resposta do servidor:', response);
-      // })
+      this.httpClient.post(`${enviroments.apiUrl}/auth/login`, this.loginForm.value).subscribe((response: any) => {
+        console.log('Resposta do servidor:', response);
+        localStorage.setItem('token', response.token);
+       })
     } else {
       console.log('Formulário Inválido');
       this.loginForm.markAllAsTouched();
