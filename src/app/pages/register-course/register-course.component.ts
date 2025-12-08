76fab1008401aa1361f6741644b2b7418ec36ffa
teathiago <mcrxdesigner@gmail.com>
2025-12-08 21:46:58 +0000

    feat: integrate course registration with database

--- src/app/pages/register-course/register-course.component.ts
+++ src/app/pages/register-course/register-course.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { enviroments } from '../../../enviroments/enviroments';
+
+interface Degree {
+  id: number;
+  code: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-register-course',
@@ -18,18 +26,65 @@ export class RegisterCourseComponent implements OnInit {
 
   private fb = inject(FormBuilder);
   courseForm: FormGroup;
+  showDegreeList = false;
 
-  constructor() {
+  get degrees() {
+  return this.courseForm.get('degrees') as FormArray;
+  }
+
+  constructor(private httpClient: HttpClient) {
     this.courseForm = this.fb.group({
       code: ['', Validators.required],
       name: ['', Validators.required],
-      degree: ['', Validators.required],
+      degrees: this.fb.array([], Validators.required),
       ects: [null, [Validators.required, Validators.min(1), Validators.max(8)]]
     });
   }
 
-  ngOnInit(): void {
+  newDegreeGroup(degree: Degree) {
+    return this.fb.group({
+      id: [degree.id],
+      code: [degree.code],
+      name: [degree.name]
+    });
   }
+
+  addDegree(degree: Degree) {
+    const alreadyAdded = this.degrees.controls.some(d => d.value.id === degree.id);
+    if (!alreadyAdded) {
+      this.degrees.push(this.newDegreeGroup(degree));
+    }
+    this.showDegreeList = false;
+  }
+
+  removeDegree(index: number) {
+    this.degrees.removeAt(index);
+  }
+
+  toggleDegreeList() {
+    this.showDegreeList = !this.showDegreeList;
+  }
+
+  ngOnInit(): void {
+    this.loadDegrees();
+  }
+
+  allDegrees: Degree[] = [];
+
+  loadDegrees() {
+  const token = localStorage.getItem('token');
+
+
+  this.httpClient.get<Degree[]>(`${enviroments.apiUrl}/degrees`)
+    .subscribe({
+      next: (degrees) => {
+        this.allDegrees = degrees;  
+      },
+      error: (err) => {
+        console.error('Erro ao carregar degrees:', err);
+      }
+    });
+}
 
   onSubmit() {
     if (this.courseForm.valid) {
