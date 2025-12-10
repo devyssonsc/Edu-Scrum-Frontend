21a5e7891669ed19e09a91974c2c32809bacb1ab
teathiago <mcrxdesigner@gmail.com>
2025-12-10 01:19:04 +0000

    feat: complete integration with database for Student

--- src/app/pages/register-student/register-student.component.ts
+++ src/app/pages/register-student/register-student.component.ts
@@ -23,8 +23,6 @@ export class RegisterStudentComponent{
   private fb = inject(FormBuilder);
   studentForm: FormGroup;
 
-
-
   constructor(private httpClient: HttpClient) {
     this.studentForm = this.fb.group({
       name: ['', Validators.required],
@@ -37,16 +35,24 @@ export class RegisterStudentComponent{
     });
   }
 
-    allDegrees: any[] = [
-      { id: 1, code: 'EI', name: 'Engenharia Informática' },
-      { id: 2, code: 'SIG', name: 'Sistemas de Informação para Gestão' },
-      { id: 3, code: 'RI', name: 'Relações Internacionais' },
-      { id: 4, code: 'ES', name: 'Educação Social' },
-      { id: 5, code: 'EGI', name: 'Engenharia e Gestão Industrial' }
-    ];
+  ngOnInit(): void {this.loadDegrees();}
     
-  selectedDegreeId = new FormControl<number | null>(null, Validators.required);
+  allDegrees: any[] = [];
+  loadDegrees() {
+  const token = localStorage.getItem('token');
 
+  this.httpClient.get<any[]>(`${enviroments.apiUrl}/degrees`)
+    .subscribe({
+      next: (degrees) => {
+        this.allDegrees = degrees;  
+      },
+      error: (err) => {
+        console.error('Erro ao carregar degrees:', err);
+      }
+    });
+}
+
+  selectedDegreeId = new FormControl<number | null>(null, Validators.required);
   selectDegree() {
     this.selectedDegreeId.markAsTouched();
 
@@ -55,25 +61,24 @@ export class RegisterStudentComponent{
     }
     
     const degreeId = Number(this.selectedDegreeId.value);
-    const selectedDegree = this.allDegrees.find(c => c.id === degreeId);
-
-    if (!selectedDegree) return;
-
-    this.studentForm.get("degree")?.setValue(selectedDegree);
+    this.studentForm.get("degree")?.setValue(degreeId);
   }
 
 
   onSubmit() {
     if (this.studentForm.valid) {
 
-      //linha temporaria, depois é pra validar se um curso foi colocado ou não
-      this.studentForm.value.degree = null
       const firstName = this.studentForm.value.name.trim().split(' ')[0];
       this.studentForm.value.password = firstName + "12345"
 
       console.log('Formulário Válido (Student):', this.studentForm.value);
-      this.httpClient.post(`${enviroments.apiUrl}/users`, this.studentForm.value).subscribe(response => {
+      this.httpClient.post(`${enviroments.apiUrl}/users`, this.studentForm.value).subscribe((response: any) => {
               console.log('Resposta do servidor:', response);
+
+              this.httpClient.post(`${enviroments.apiUrl}/degrees/${this.studentForm.value.degree}/students/${response.id}`, this.studentForm.value).subscribe(response => {
+              console.log('Resposta do servidor:', response);
+              })
+            
             })
     } else {
       console.log('Formulário Inválido');
