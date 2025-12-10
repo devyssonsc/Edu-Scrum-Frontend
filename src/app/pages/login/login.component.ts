cb054f0871afebd3a1cf917c20d09ad734282b2c
teathiago <mcrxdesigner@gmail.com>
2025-12-10 01:47:11 +0000

    feat: redirect User to role Dashboard after Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { enviroments } from '../../../enviroments/enviroments';
 
 @Component({
@@ -12,6 +13,7 @@ import { enviroments } from '../../../enviroments/enviroments';
 })
 export class LoginComponent {
   private fb = inject(FormBuilder);
+  private router = inject(Router);
 
 
   loginForm = this.fb.group({
@@ -23,12 +25,27 @@ export class LoginComponent {
     
   }
 
+  redirectUser(role: string){
+    switch (role) {
+      case 'ADMIN':
+        this.router.navigate(['/admin-dashboard']);
+        break;
+      case 'TEACHER':
+        this.router.navigate(['/teacher-dashboard']);
+        break;
+      case 'STUDENT':
+        alert('Dashboard em desenvolvimento.');
+        //this.router.navigate(['/student-dashboard']);
+        break;
+  }
+  }
   onSubmit() {
     if (this.loginForm.valid) {
       console.log('Formulário Válido (Login):', this.loginForm.value);
       this.httpClient.post(`${enviroments.apiUrl}/auth/login`, this.loginForm.value).subscribe((response: any) => {
         console.log('Resposta do servidor:', response);
         localStorage.setItem('token', response.token);
+        this.redirectUser(response.role);
        })
        
     } else {
