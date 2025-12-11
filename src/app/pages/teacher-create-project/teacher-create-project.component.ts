2655ce6a418c8b4bc9511c74f0514e2cab311009
teathiago <mcrxdesigner@gmail.com>
2025-12-11 02:12:58 +0000

    feat: complete integration with database for Project

--- src/app/pages/teacher-create-project/teacher-create-project.component.ts
+++ src/app/pages/teacher-create-project/teacher-create-project.component.ts
@@ -1,8 +1,11 @@
+
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { DataService } from '../../services/dataService';
+import { DataService, Course } from '../../services/dataService';
+import { HttpClient } from '@angular/common/http';
+import { enviroments } from '../../../enviroments/enviroments';
 
 @Component({
   selector: 'app-teacher-create-project',
@@ -19,28 +22,36 @@ export class TeacherCreateProjectComponent implements OnInit {
   private dataService = inject(DataService);
 
   projectForm: FormGroup;
-  courseId: string | null = null;
+  courseId: number | null = null; 
   courseName: string = '';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.projectForm = this.fb.group({
       name: ['', Validators.required],
       startDate: ['', Validators.required],
       endDate: ['', Validators.required],
-      description: [''] // Campo opcional
+      description: ['']
     });
   }
 
   ngOnInit() {
-    this.courseId = this.route.snapshot.paramMap.get('courseId');
-    
+
+    const rawId = this.route.snapshot.paramMap.get('courseId');
+
     const state = history.state;
     if (state && state.courseName) {
       this.courseName = state.courseName;
-    } else if (this.courseId) {
-      this.dataService.getCourseByCode(this.courseId).subscribe(c => {
-        this.courseName = c ? c.name : this.courseId!;
-      });
+    }
+
+    if (rawId) {
+      this.courseId = Number(rawId);
+
+      if (!this.courseName && !isNaN(this.courseId)) {
+
+        this.dataService.getCourseById(this.courseId).subscribe((c: Course | undefined) => {
+          this.courseName = c ? c.name : 'Unknown Course';
+        });
+      }
     }
   }
 
@@ -53,24 +64,27 @@ export class TeacherCreateProjectComponent implements OnInit {
         alert('End date must be after start date.');
         return;
       }
-
-      // Constrói o objeto com TODOS os campos
       const newProject = {
         name: this.projectForm.value.name,
-        courseCode: this.courseId,
+        courseId: this.courseId,
         startDate: this.projectForm.value.startDate,
         endDate: this.projectForm.value.endDate,
-        description: this.projectForm.value.description // Garante que a descrição vai aqui
+        description: this.projectForm.value.description
       };
+      
+      console.log('Formulário Válido:', this.projectForm.value);
+      this.httpClient.post(`${enviroments.apiUrl}/courses/${newProject.courseId}/projects`, newProject).subscribe((response: any) => {
+      console.log("Resposta do Servidor:" + response)
+
 
       this.dataService.createProject(newProject).subscribe((success: boolean) => {
         if (success) {
           alert('Project created successfully!');
-          // Redireciona para a lista da cadeira
           this.router.navigate(['/teacher-dashboard/course', this.courseId]);
         }
-      });
 
+      });
+    });
     } else {
       this.projectForm.markAllAsTouched();
     }
