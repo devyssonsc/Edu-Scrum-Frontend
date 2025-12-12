d3af7f4ed9ded87a979af5cd0ddfe321ef7c2f66
TigasMigas22 <tiagopnsilva@gmail.com>
2025-12-12 00:57:51 +0000

    fix: change type of card's input to string

--- src/app/pages/teacher-course-detail/teacher-course-detail.component.ts
+++ src/app/pages/teacher-course-detail/teacher-course-detail.component.ts
@@ -13,6 +13,9 @@ import { DataService, Course, Project } from '../../services/dataService';
   styleUrl: './teacher-course-detail.component.scss'
 })
 export class TeacherCourseDetailComponent implements OnInit {
+String(arg0: number): string {
+throw new Error('Method not implemented.');
+}
 
   private route = inject(ActivatedRoute);
   private router = inject(Router);
